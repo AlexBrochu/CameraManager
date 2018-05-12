5882345b8aea637a8d92f3b9781d76483086c283
AlexBrochu <alexandre.brochu.1@ens.etsmtl.ca>
2018-05-12 10:45:04 -0400

    Setup dashboard page

--- ONVIF_MediaProfilDashboard/ConfigDashboard.xaml.cs
+++ ONVIF_MediaProfilDashboard/ConfigDashboard.xaml.cs
@@ -27,5 +27,10 @@ namespace ONVIF_MediaProfilDashboard
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
